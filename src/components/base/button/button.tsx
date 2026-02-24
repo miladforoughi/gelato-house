70ef9c476f7270ef40f5c587415d49869c25dc47
Milad Foroughi <miladpro2020@gmail.com>
2026-02-24 20:45:12 +0330

    create reusable button component

---
+++ src/components/base/button/button.tsx
@@ -0,0 +1,49 @@
+import type { JSX } from 'react'
+
+type ButtonProps = {
+  variant?: 'contained' | 'outlined' | 'text'
+  colors?: 'primary' | 'secondary'
+  size?: 'sm' | 'md' | 'lg'
+} & React.PropsWithChildren &
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+
+const buttonBaseStyle = 'rounded-3xl'
+
+const variants = {
+  contained: {
+    primary: 'bg-primary-500 text-white',
+    secondary: 'bg-secondary-500 text-white',
+  },
+  outlined: {
+    primary: 'text-primary-500 border border-primary-500',
+    secondary: 'text-secondary-500 border border-secondary-500',
+  },
+  text: {
+    primary: 'text-gray-900',
+    secondary: 'text-gray-900',
+  },
+}
+
+const sizes = {
+  sm: 'px-2 py-1 text-sm',
+  md: 'px-4 py-2 text-base',
+  lg: 'px-6 py-3 text-lg',
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  variant = 'contained',
+  className = '',
+  color = 'primary',
+  size = 'md',
+  children,
+  ...props
+}): JSX.Element => {
+  return (
+    <button
+      className={`${buttonBaseStyle} ${variants[variant][color]} ${sizes[size]} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,9 @@
+import { Button } from '../base/button/button'
 export const Header: React.FC = () => {
   return (
     <header>
       <h1>Header</h1>
+      <Button variant='contained'>Milad Foroughi</Button>
     </header>
   )
 }
